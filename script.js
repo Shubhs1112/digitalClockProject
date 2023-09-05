0b4908e940577859421fcdf1ade303a0d51ef17f
Shubham Katwale <76983578+Shubhs1112@users.noreply.github.com>
2023-09-05 17:07:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ let sec = document.getElementById("sec");
 setInterval(() => {
     let currTime = new Date();
     hrs.innerHTML = (currTime.getHours() < 10 ? '0' : '') + currTime.getHours();
-    min.innerHTML = (currTime.getMinutes() < 10 ? '0' : ') + currTime.getMinutes();
+    min.innerHTML = (currTime.getMinutes() < 10 ? '0' : '') + currTime.getMinutes();
     sec.innerHTML = (currTime.getSeconds() < 10 ? '0' : '') + currTime.getSeconds();
 },1000);
